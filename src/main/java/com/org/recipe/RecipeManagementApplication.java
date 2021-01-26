5aecad2e189f43093d562b15933a649bce08d4ca
smita28 <smitadevi7@gmail.com>
2021-01-26 12:10:55 +0530

    final commit

--- src/main/java/com/org/recipe/RecipeManagementApplication.java
+++ src/main/java/com/org/recipe/RecipeManagementApplication.java
@@ -2,8 +2,6 @@ package com.org.recipe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -24,10 +22,5 @@ public class RecipeManagementApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RecipeManagementApplication.class, args);
 	}
-
-	@Bean
-    MethodValidationPostProcessor methodValidationPostProcessor() {
-        return new MethodValidationPostProcessor();
-    }
 
 }
